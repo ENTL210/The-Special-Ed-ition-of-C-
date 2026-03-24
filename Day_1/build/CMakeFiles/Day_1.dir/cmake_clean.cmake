86182e250942e30805b160ccdba04b848029b001
entl210 <edwardlamhitech@gmail.com>
2026-03-24 14:42:40 -0700

    Learning about Cmake.

---
+++ Day_1/build/CMakeFiles/Day_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day_1.dir/main.cpp.o"
+  "CMakeFiles/Day_1.dir/main.cpp.o.d"
+  "Day_1"
+  "Day_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
